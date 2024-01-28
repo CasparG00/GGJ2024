55e18585162c252c8178129138ac48dbaa59b24e
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-01-28 13:32:29 +0100

    reïmplemented menu

--- Assets/Entities/UI/SceneSwitch.cs
+++ Assets/Entities/UI/SceneSwitch.cs
@@ -1,10 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadScene(1);
